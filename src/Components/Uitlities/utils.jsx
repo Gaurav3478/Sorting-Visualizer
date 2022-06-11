1ac2512f01507429388da9f1208ac434edb79c8c
Gaurav <gaurav.t2002@gmail.com>
2022-06-11 15:37:16 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/Components/Uitlities/utils.jsx
@@ -0,0 +1,35 @@
+export function Swap(x, y) {
+	const temp = x.style.height;
+	x.style.height = y.style.height;
+	y.style.height = x = temp;
+}
+
+export function MakeDelay(milisec) {
+	return new Promise(resolve => {
+		setTimeout(() => { resolve('') }, milisec);
+	})
+}
+
+export var delay = 200
+export const PRIMARY_COLOR = 'rgb(99, 156, 255)'; //light blue
+export const FINAL_COLOR = 'rgb(50,205,50)';
+export const POSITION_FINAL_COLOR = 'rgb(183, 51, 245)'; //purple
+export const COMPARE_COLOR = 'blue';
+export const SWAP_COLOR = 'red';
+export const MIN_COLOR = 'rgb(255, 112, 31)' //orange-ish
+
+export function randomNumberFrom(l, r) {
+	return Math.floor(Math.random() * (l - r + 1) + r) //generates random number between l and r both inclusive
+}
+
+export function disableAllButtons(val) {
+	document.getElementById('rangeSlider').disabled = val;
+	var btns = document.querySelectorAll(".btn")
+	for (var i = 0; i < btns.length; i++) {
+		btns[i].disabled = val;
+	}
+}
+
+export function changeDelay(val) {
+	delay = val;
+}
